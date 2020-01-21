7ea3398df48ae6a7e6ceebdacfdcfb37690eed60
Tom Slater <thomas.a.slater@gmail.com>
2020-01-21 14:30:28 -0500

    commenting out old code

--- main.mjs
+++ main.mjs
@@ -1,13 +1,19 @@
-import { Pendulum } from "./modules/pendulum.mjs"
+// import { Pendulum } from "./modules/pendulum.mjs"
 import showPendulum from "./modules/showPendulum.mjs"
 import global from "./modules/parameters.mjs"
-// importScripts("./parameters.js")
 
 // tf.setBackend("webgl")
 
 let wh
 let pendulumCanvas = document.getElementById("pendulum-canvas")
 let ctx = pendulumCanvas.getContext("2d")
+
+let ep_step = 0,
+  experience,
+  episode = 0
+// const pendulum = new Pendulum()
+// const ddpg_worker = new Worker("./modules/ddpg_worker.js")
+const animationQueue = []
 
 function resizeCanvas() {
   let navbarHeight = document.getElementById("navbar").offsetHeight
@@ -37,37 +43,32 @@ window.onresize = resizeCanvas
 //   controlsDiv.style.display = divs[0].style.display == "none" ? "block" : "none"
 // }
 
-let ep_step = 0,
-  experience,
-  episode = 0
-const pendulum = new Pendulum()
-const ddpg_worker = new Worker("./modules/ddpg_worker.js")
-ddpg_worker.postMessage({
-  settings: {
-    state_len: pendulum.state.length,
-    global: Object.assign({}, global)
-  }
-})
+// ddpg_worker.postMessage({
+//   settings: {
+//     state_len: pendulum.state.length,
+//     global: Object.assign({}, global)
+//   }
+// })
 
-ddpg_worker.addEventListener("message", e => {
-  const action = e.data.action
-  experience = pendulum.update(action)
+// ddpg_worker.addEventListener("message", e => {
+//   const action = e.data.action
+//   experience = pendulum.update(action)
 
-  requestAnimationFrame(() => {
-    ddpg_worker.postMessage({ experience: experience, ep_step: ep_step })
-    pendulum.show(ctx, wh)
-  })
-  if (ep_step >= global.ep_steps) {
-    ep_step = 0
-    episode++
-    pendulum.reset()
-  }
-  ep_step++
-})
+//   requestAnimationFrame(() => {
+//     ddpg_worker.postMessage({ experience: experience, ep_step: ep_step })
+//     pendulum.show(ctx, wh)
+//   })
+//   if (ep_step >= global.ep_steps) {
+//     ep_step = 0
+//     episode++
+//     pendulum.reset()
+//   }
+//   ep_step++
+// })
 
-requestAnimationFrame(() => {
-  pendulum.show(ctx, wh)
-})
-experience = pendulum.update(0)
-ddpg_worker.postMessage({ experience: experience, ep_step: ep_step })
-ep_step++
+// requestAnimationFrame(() => {
+//   pendulum.show(ctx, wh)
+// })
+// experience = pendulum.update(0)
+// ddpg_worker.postMessage({ experience: experience, ep_step: ep_step })
+// ep_step++
